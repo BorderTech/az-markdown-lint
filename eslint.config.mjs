7c95adbebec1fa437f7170cfab65a4c74a695795
Rick BROWN <rick.brown@homeaffairs.gov.au>
2025-08-04 12:19:42 +1000

    Add eslint to check code style

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+	{
+		ignores: ['node_modules/**', 'dist/**', '.history/**'],
+
+		// Rules to apply.
+		rules: {
+			'@stylistic/semi': ['error', 'always'],
+			'no-unused-vars': 'error',
+			'@stylistic/indent': ['error', 'tab'],
+			'@stylistic/spaced-comment': ['error', 'always'],
+			'@stylistic/keyword-spacing': ['error', { 'before': true }],
+			'@stylistic/quotes': ['error', 'single', { 'avoidEscape': true }],
+			'@stylistic/space-before-blocks': ['error', 'always'],
+			'@stylistic/space-infix-ops': 'error',
+			'@stylistic/brace-style': ['error', '1tbs'],
+			'@stylistic/comma-style': ['error', 'last'],
+			'@stylistic/no-trailing-spaces': 'error',
+			'@stylistic/space-before-function-paren': ['error', 'always'],
+			'@stylistic/no-multi-spaces': 'error',
+			'@stylistic/object-curly-spacing': ['error', 'always'],
+			'@stylistic/arrow-spacing': ['error', { 'before': true, 'after': true }],
+			'@stylistic/eol-last': ['error', 'always']
+		},
+		plugins: {
+			'@stylistic': stylistic
+		}
+	}
+];
