40563ec5adb16ccbf0580610b849635a33a3408f
jarrah999 <Jarrah999@users.noreply.github.com>
2025-08-07 11:38:24 +1000

    update to index.mjs code

--- task/index.mjs
+++ task/index.mjs
@@ -1,7 +1,5 @@
-import * as markdownlint from 'markdownlint';
 import * as glob from 'glob';
-import { lint } from 'markdownlint/promise' ;
-import { lint as lintPromise } from "markdownlint/promise";
+import { lint as lintPromise } from 'markdownlint/promise';
 import * as tl from 'azure-pipelines-task-lib/task.js';
 
 
@@ -9,7 +7,7 @@ const pattern = process.argv[2] || '**.md'; // should be users markdown files wh
 const files = glob.sync(pattern); // glob will work its magic and find the users markdown documents
 
 if (files.length == 0) {
-	console.warn (' No lines to lint 🔍:' );
+	console.warn(' No lines to lint 🔍:');
 	process.exit(0);
 }
 
@@ -26,21 +24,20 @@ const options = {
 const results = lintPromise(options);
 results.then(handleresults);
 function handleresults(lintresults) {
-	console.dir(lintresults, { "colors": true, "depth": null });
-} 
+	console.dir(lintresults, { 'colors': true, 'depth': null });
+}
 
 async function run() {
 	try {
-         const inputString = tl.getInput('pattern', false);
-		  if (inputString == 'bad') {
-             tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
-             return;
-         }
-		 console.log('linted results :\n', inputString );
+		const inputString = tl.getInput('pattern', false);
+		if (inputString == 'bad') {
+			tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+			return;
 		}
-		    catch (error) {
-         tl.setResult(tl.TaskResult.Failed, err.message);
-     }
- }
+		console.log('linted results :\n', inputString);
+	} catch(err) {
+		tl.setResult(tl.TaskResult.Failed, err.message);
+	}
+}
 
- run();
+run();
