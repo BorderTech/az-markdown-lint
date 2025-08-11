f5bc4324a1b368f938b07000a3e75eb707b3b701
Rick BROWN <rick.brown@homeaffairs.gov.au>
2025-08-11 15:12:52 +1000

    Fix up the unit tests

--- tests/bad_markdown_with_config.js
+++ tests/bad_markdown_with_config.js
@@ -2,10 +2,11 @@
 	This is a mock task run, it is expected to be run from _suite.js,
 	which also contains assertions etc.
  */
+const path = require('path');
 const tmrm = require('azure-pipelines-task-lib/mock-run');
 const { getTaskPath } = require('./testUtils');
 
 let tmr = new tmrm.TaskMockRunner(getTaskPath());
-tmr.setInput('pattern', 'tests/resources/bad/line-length.md');
+tmr.setInput('pattern', path.join(tl.cwd(), 'tests/resources/bad/line-length.md'));
 tmr.setInput('config', 'tests/resources/.markdownlint.jsonc');
 tmr.run();
