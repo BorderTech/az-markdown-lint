51f761e66352cba37e1375a40515351140d30da3
jarrah999 <Jarrah999@users.noreply.github.com>
2025-08-25 10:42:46 +1000

    updated index.js code

--- task/index.js
+++ task/index.js
@@ -1,10 +1,10 @@
 const glob = require('glob');
 const { lintpromise } = require('markdownlint/promise');
-const tl = require('azure-pipelines-task-lib/task.js');
+const tl = require('azure-pipelines-task-lib/markdown-lint.js');
 
 
 function files() {
-	const pattern = rocess.argv[2] || '**.md';
+	const pattern = process.argv[2] || '**.md';
 	return glob.sync(pattern);
 	if (files.length == 0) {
 		console.warn(' No lines to lint 🔍:');
@@ -26,34 +26,42 @@ function options() {
 	};
 };
 
-function linter() {
+
+
+function lint() {
 	const results = lintpromise(options);
 	results.then(handleresults);
 	function handleresults(lintresults) {
 		console.dir(lintresults, { 'colors': true, 'depth': null });
 	};
-	return (linter);
-
+	return lint;
 };
 
 
 async function run() {
+	const check = new lint();
+
+
 	try {
 		const inputString = tl.getInput('pattern', false);
-		if (inputString == 'bad') {
-			tl.setResult(tl.TaskResult.Failed, 'Bad input was given');
+		if ( inputstring = check.lintresults[files].length > 0 ) {
+			tl.setResult(tl.TaskResult.Failed, 'Bad input was given: \n', lintresults);
 			return;
+		} else {
+			tl.setResults(tl.TaskResult.Succeeded, 'linted results :\n', inputString, lintresults);
 		}
-		console.log('linted results :\n', inputString);
 	} catch(err) {
 		tl.setResult(tl.TaskResult.Failed, err.message);
+		console.err(' something went wronge ', err.message);
+
 
 	};
 
 };
 
-module.exports = { run };
-module.exports = { files };
+run();
+
+
 
 
 
